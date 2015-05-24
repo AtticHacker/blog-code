fe71b5cf8e87502aea2538dfe24293bbcf9b9c89
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2015-05-24 18:55:48 +0200

    update code

--- 15-05-23/Part5.elm
+++ 15-05-23/Part5.elm
@@ -12,11 +12,10 @@ displayFPS f = show <| "Current FPS: " ++ (toString <| round <| 1000 / f)
 main = render
     <~ Signal.map (show << heavy) (Signal.foldp (\_ s -> s + 1) 0 Mouse.clicks)
     ~ Time.fps fpsCap
-    ~ Signal.foldp (\_ s -> s + 1) 0 Mouse.clicks
 
 heavy : Int -> List Int
 heavy i = List.repeat 10000 i
 
-render : Element -> Float -> Int -> Element
-render h f mouse = flow down
+render : Element -> Float -> Element
+render h f = flow down
     [ displayFPS f, h ]
--- 15-05-23/Part6.elm
+++ 15-05-23/Part6.elm
@@ -1,31 +1,7 @@
 import Graphics.Element exposing (..)
 import Signal exposing ((<~), (~))
-import Task
 import Time
-import Keyboard
 import Mouse
-
-mouseBox : Signal.Mailbox Int
-mouseBox = Signal.mailbox 0
-
-spaceBox : Signal.Mailbox Int
-spaceBox = Signal.mailbox 0
-
-heavyBox : Signal.Mailbox Element
-heavyBox = Signal.mailbox empty
-
-port updateMouseBox : Signal (Task.Task x ())
-port updateMouseBox = Signal.sampleOn Mouse.clicks
-    <| (Signal.send mouseBox.address << ((+) 1)) <~ mouseBox.signal
-
-port updateSpaceBox : Signal (Task.Task x ())
-port updateSpaceBox = Signal.sampleOn Keyboard.space
-    <| (Signal.send spaceBox.address << ((+) 1)) <~ spaceBox.signal
-
-port updateHeavyBox : Signal (Task.Task x ())
-port updateHeavyBox = Signal.sampleOn Mouse.clicks
-    <| Signal.send heavyBox.address << show << heavy
-    <~ ((+) <~ mouseBox.signal ~ spaceBox.signal)
 
 fpsCap : Int
 fpsCap = 30
@@ -36,16 +12,51 @@ displayFPS f = show <| "Current FPS: " ++ (toString <| round <| 1000 / f)
 heavy : Int -> List Int
 heavy i = List.repeat 10000 i
 
-main : Signal Element
-main = render <~ Time.fps fpsCap
-    ~ heavyBox.signal
-    ~ mouseBox.signal
-    ~ spaceBox.signal
+type UpdateTick
+    = TimeTick Float
+    | MouseTick ()
 
-render : Float -> Element -> Int -> Int -> Element
-render f h mouse space = flow down
-    [ displayFPS f
-    , h
-    , flow right [show "Mouse:", show mouse]
-    , flow right [show "Space:", show space]
+type alias State =
+    { fps : Float
+    , clicks : Int
+    , view :
+        { heavy : Element
+        }
+    }
+
+main : Signal Element
+main = render <~ Signal.foldp update initState inputs
+
+initState : State
+initState =
+    { fps = 0
+    , clicks = 0
+    , view =
+        { heavy = show <| heavy 0
+        }
+    }
+
+update : UpdateTick -> State -> State
+update input state = case input of
+    TimeTick fps -> {state | fps <- fps}
+    MouseTick () -> let
+        c = state.clicks
+        v = state.view in
+        { state
+            | clicks <- c + 1
+            , view  <- {v | heavy <- show <| heavy <| 1 + state.clicks }
+            }
+    _ -> state
+
+inputs : Signal UpdateTick
+inputs = Signal.mergeMany
+    [ TimeTick <~ Time.fps fpsCap
+    , MouseTick <~ Mouse.clicks
+    ]
+
+render : State -> Element
+render state = flow down
+    [ displayFPS state.fps
+    , state.view.heavy
+    , flow right [show "Mouse:", show state.clicks]
     ]
